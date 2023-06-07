42cb2d871ba1491dac20b9ebd12762c2a7e7c37f
OURABIG <94923157+OURABIG@users.noreply.github.com>
2023-06-07 10:20:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <SFML/Graphics.hpp>
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML TUTORIAL");
+    sf::CircleShape shape(200.f);
     shape.setFillColor(sf::Color::Blue);
 
     while (window.isOpen())
